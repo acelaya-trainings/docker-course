4c8435875afddc9abafd15aca7bf3bd9b7a1c93b
alejandro <alejandrocelaya@gmail.com>
2017-04-04 20:13:11 +0200

    Updated bootstrap script to deploy app

--- 02/bootstrap.sh
+++ 02/bootstrap.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-wget $WEBAPP_URL
-unzip webapp.zip
-ln -sf /var/www/html webapp
+wget $WEBAPP_URL -O /var/www/webapp.zip
+cd /var/www
+unzip /var/www/webapp.zip
+rm -rf /var/www/html
+mv /var/www/webapp /var/www/html
 
 apachectl -DFOREGROUND
 
